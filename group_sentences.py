e55cb1eb581a8a38d5a6e70f0fed71eb8f74c93b
Chen Yufei <yufei.chen@pku.edu.cn>
2018-06-27 12:09:48 +0800

    group_sentences: optimize for speed

--- group_sentences.py
+++ group_sentences.py
@@ -2,6 +2,8 @@ import pickle
 
 import numpy as np
 
+from logger import logger
+
 
 def group_sentences(length_counter, n_buckets):
     if len(length_counter) < n_buckets:
@@ -9,6 +11,14 @@ def group_sentences(length_counter, n_buckets):
     lengths = [0] + sorted(length_counter)
     dp = np.zeros((len(lengths), n_buckets + 1), dtype=np.int64)
     bp = np.zeros((len(lengths), n_buckets + 1), dtype=np.int64)
+
+    sigma_length_count = [0]
+    sigma_length_x_count = [0]
+    for length in lengths[1:]:
+        sigma_length_count.append(sigma_length_count[-1] + length_counter[length])
+        sigma_length_x_count.append(sigma_length_x_count[-1] + length * length_counter[length])
+
+
     dp[0, 1:] = 2 ** 63 - 1
     for bucket_count in range(1, n_buckets + 1):
         for max_length_idx in range(bucket_count, len(lengths)):
@@ -22,8 +32,13 @@ def group_sentences(length_counter, n_buckets):
                     continue
                 this_answer = \
                     dp[previous_length_idx, bucket_count - 1]
-                for k in range(previous_length_idx + 1, max_length_idx):
-                    this_answer += length_counter[lengths[k]] * (max_length - lengths[k])
+                intermediate_length_count = max_length_idx - previous_length_idx - 1
+                this_answer += (sigma_length_count[max_length_idx] -
+                                sigma_length_count[previous_length_idx]) * max_length
+                this_answer -= sigma_length_x_count[max_length_idx] - \
+                               sigma_length_count[previous_length_idx]
+                # for k in range(previous_length_idx + 1, max_length_idx):
+                #     this_answer += length_counter[lengths[k]] * (max_length - lengths[k])
                 if this_answer < min_answer:
                     min_answer = this_answer
                     min_previous_length_idx = previous_length_idx
