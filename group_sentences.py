8d2ffc60e2657e0203c916fca02b26447a58782c
Chen Yufei <yufei.chen@pku.edu.cn>
2018-08-15 15:34:59 +0800

    group_sentences: consider batch size

--- group_sentences.py
+++ group_sentences.py
@@ -5,38 +5,43 @@ import numpy as np
 from logger import logger
 
 
-def group_sentences(length_counter, n_buckets):
+def group_sentences(length_counter, n_buckets, batch_size):
     if len(length_counter) < n_buckets:
         n_buckets = len(length_counter)
     lengths = [0] + sorted(length_counter)
+    # dp[0, 0] = 0
+    # dp[len_idx, n_bkt] (len_idx >= n_bkt when len_idx > 0)
     dp = np.zeros((len(lengths), n_buckets + 1), dtype=np.int64)
     bp = np.zeros((len(lengths), n_buckets + 1), dtype=np.int64)
 
-    sigma_length_count = [0]
+    sigma_count = [0]
     sigma_length_x_count = [0]
     for length in lengths[1:]:
-        sigma_length_count.append(sigma_length_count[-1] + length_counter[length])
+        sigma_count.append(sigma_count[-1] + length_counter[length])
         sigma_length_x_count.append(sigma_length_x_count[-1] + length * length_counter[length])
 
-
-    dp[0, 1:] = 2 ** 63 - 1
     for bucket_count in range(1, n_buckets + 1):
+        # length_idx >= n_bkt
         for max_length_idx in range(bucket_count, len(lengths)):
             max_length = lengths[max_length_idx]
             min_answer = 2 ** 63 - 1
             min_previous_length_idx = -1
-            for previous_length_idx in range(max_length_idx):
-                if previous_length_idx == 0 and bucket_count - 1 != 0:
-                    continue
+            # previous_length_idx >= n_bkt - 1
+            for previous_length_idx in range(bucket_count - 1, max_length_idx):
+                # if previous_length_idx == 0 and bucket_count - 1 != 0:
+                #     continue
                 if previous_length_idx != 0 and bucket_count - 1 == 0:
                     continue
                 this_answer = \
                     dp[previous_length_idx, bucket_count - 1]
-                intermediate_length_count = max_length_idx - previous_length_idx - 1
-                this_answer += (sigma_length_count[max_length_idx] -
-                                sigma_length_count[previous_length_idx]) * max_length
+                range_sent_count = sigma_count[max_length_idx] - \
+                                   sigma_count[previous_length_idx]
+                this_answer += range_sent_count * max_length
                 this_answer -= sigma_length_x_count[max_length_idx] - \
-                               sigma_length_count[previous_length_idx]
+                               sigma_length_x_count[previous_length_idx]
+                this_sent_batch_size = batch_size // max_length
+                dust_sent_size = this_sent_batch_size - range_sent_count % this_sent_batch_size
+                this_answer += dust_sent_size * max_length
                 # for k in range(previous_length_idx + 1, max_length_idx):
                 #     this_answer += length_counter[lengths[k]] * (max_length - lengths[k])
                 if this_answer < min_answer:
@@ -48,9 +53,12 @@ def group_sentences(length_counter, n_buckets):
             # print("dp[{}, {}] = {} <- {}".format(lengths[max_length_idx], bucket_count, min_answer,
             #                                      lengths[min_previous_length_idx]))
 
+    ret = [lengths[-1]]
     max_length_idx = len(lengths) - 1
-    ret = [lengths[max_length_idx]]
-    bucket_count = n_buckets
+    min_answer, best_bucket_count = min(
+        (dp[max_length_idx, bucket_count], bucket_count)
+        for bucket_count in range(1, n_buckets + 1))
+    bucket_count = best_bucket_count
     while bucket_count > 1:
         max_length_idx = bp[max_length_idx, bucket_count]
         ret.append(lengths[max_length_idx])
